239bb4a63236dd17dd872a9137b27569aa8a2b13
Nenad Labudovic <holovision@Nenads-Mac-Studio.local>
2025-08-21 21:48:28 +0200

    ruting 1

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,13 +5,28 @@ import bcrypt from "bcryptjs";
 const generateToken = (id) => jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "7d" });
 
 export const registerUser = async (req, res) => {
-  const { email, password, isAdmin  } = req.body;
-  const userExists = await User.findOne({ email });
-  if (userExists) return res.status(400).json({ message: "User already exists" });
+  const { email, password } = req.body;
+  const existingUser = await User.findOne({ email });
+
+  if (existingUser)
+    return res.status(400).json({ message: "User already exists" });
 
   const hashedPassword = await bcrypt.hash(password, 10);
-  const user = await User.create({ email, password: hashedPassword, isAdmin });
-  res.status(201).json({ token: generateToken(user._id) });
+  const newUser = await User.create({
+    email,
+    password: hashedPassword,
+  });
+
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+
+  res.status(201).json({
+    token,
+    email: newUser.email,
+    subscription: newUser.subscription,
+    isAdmin: newUser.isAdmin,
+    usageMonth: newUser.usageMonth,
+    monthlyUsageMinutes: newUser.monthlyUsageMinutes,
+  });
 };
 
 export const loginUser = async (req, res) => {
