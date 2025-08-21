dacd7b8c34cd55d6c9bc4a654a875a438414fb0f
Nenad Labudovic <holovision@Nenads-Mac-Studio.local>
2025-08-21 22:28:47 +0200

    ruting 5

--- frontend/src/components/LoginRegister.jsx
+++ frontend/src/components/LoginRegister.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const BACKEND_URL = "https://holovision-avatar-app-1.onrender.com";
+const BACKEND_URL = "https://holovision-avatar-app.onrender.com";
 
 const LoginRegister = () => {
 console.log("🟢 LoginRegister komponenta učitana");
