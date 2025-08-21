e47f7101b8f50dc2d88c80a60402308057789b73
Nenad Labudovic <holovision@Nenads-Mac-Studio.local>
2025-08-21 15:04:45 +0200

    Dodato admin proveravanje i zaštita ruta

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -38,7 +38,7 @@ router.get("/avatar/use", verifyToken, requireTier(["silver", "gold"]), (req, re
 // Dohvatanje podataka o korisniku preko tokena
 router.get("/me", verifyToken, async (req, res) => {
   try {
-    const user = await User.findById(req.user.id).select("-password");
+    const user = await User.findById(req.user.id).select("-password isAdmin");
     res.status(200).json(user);
   } catch (error) {
     res.status(500).json({ message: "Server error" });
