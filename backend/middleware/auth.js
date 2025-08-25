cf34b60b2edaa576ab5861f747ae4ce26f35f3c5
Nenad Labudovic <holovision@Nenads-Mac-Studio.local>
2025-08-25 15:03:46 +0200

    Add recharts 131

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,6 +2,7 @@ import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
 export const authMiddleware = async (req, res, next) => {
+    console.log("Auth.js midelvare radi:", req.user); // ovo će pokazati ID i email logovanog admina
   const token = req.headers.authorization?.split(" ")[1];
 
   if (!token) {
