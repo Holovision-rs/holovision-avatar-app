a34480b3742e1e90ba68de4294667b5d298c0adb
Nenad Labudovic <holovision@Nenads-Mac-Studio.local>
2025-08-27 14:23:36 +0200

    Add recharts 199

--- frontend/src/components/Avatar.jsx
+++ frontend/src/components/Avatar.jsx
@@ -35,7 +35,7 @@ export function Avatar(props) {
   const [setupMode, setSetupMode] = useState(false);
  
   useSessionTimer(true, token);      // ⏱️ radi usage tracking
-  //useSubscriptionCheck();            // ✅ aktivira proveru pretplate
+  
     console.log("✅ Refreshed Avatar");
 
   // ⏯️ Promena animacija kada dođe poruka
