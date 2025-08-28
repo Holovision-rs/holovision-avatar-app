e473c8ac0f5212b0e8b0d13de7791355aa0e6049
Nenad Labudovic <holovision@Nenads-Mac-Studio.local>
2025-08-29 00:00:48 +0200

    Add recharts 289

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,7 +6,14 @@ const Navbar = () => {
 
   return (
     <nav style={{ display: "flex", justifyContent: "space-between", padding: "10px" }}>
-      <span>👤 {user?.email}</span>
+      <span> {user?.name}</span>
+      <span className="flex justify-center mb-4">
+        <img
+          src={`https://api.dicebear.com/7.x/bottts/svg?seed=${avatarSeed}`}
+          alt="Avatar"
+          className="w-20 h-20 mx-auto mb-6"
+        />
+      </span>
       <button onClick={logout}>Logout</button>
     </nav>
   );
