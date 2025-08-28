79453cf80e075126876e457fc40372867a9e18b8
Nenad Labudovic <holovision@Nenads-Mac-Studio.local>
2025-08-28 23:01:01 +0200

    Add recharts 277

--- frontend/src/pages/desktop/LoginRegisterDesktop.jsx
+++ frontend/src/pages/desktop/LoginRegisterDesktop.jsx
@@ -163,6 +163,9 @@ const LoginRegister = () => {
           alt="Futuristic Robot"
           className="max-h-[90%] object-contain"
         />
+          <h1 className="absolute text-white font-bold text-6xl top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+              Welcome.
+          </h1>
       </div>
     </div>
   );
