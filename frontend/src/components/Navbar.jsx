57c7640fea0ed6009c3c45a52e0933d8c557dcbe
Nenad Labudovic <holovision@Nenads-Mac-Studio.local>
2025-09-01 00:13:48 +0200

    Add recharts 308

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { useAuth } from "../context/AuthContext";
 const Navbar = () => {
   const { user, logout } = useAuth();
   return (
-    <nav className="fixed top-0 left-1/2 transform -translate-x-1/2 w-fit md:w-2/3 z-50 bg-black px-6 py-3 rounded-lg shadow-md  w-1/2 top-0 left-1/2 ">
+    <nav className="fixed top-0 left-1/2 transform -translate-x-1/2 w-fit md:w-1/3 z-50 bg-black px-6 py-3 rounded-lg shadow-md  w-1/2 top-0 left-1/2 ">
       <span>{user?.name}</span>
       <button onClick={logout}>Logout</button>
     </nav>
