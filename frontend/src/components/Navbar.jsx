d3cba14308a1194fa20f1d44ae069cf7609ee306
Nenad Labudovic <holovision@Nenads-Mac-Studio.local>
2025-09-01 00:20:26 +0200

    Add recharts 3010

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,8 +4,8 @@ import { useAuth } from "../context/AuthContext";
 const Navbar = () => {
   const { user, logout } = useAuth();
   return (
-    <nav className="bg-gradient-to-r from-blue-600 to-purple-600 text-white fixed top-10 left-1/2 transform -translate-x-1/2 w-fit md:w-1/3 z-50 bg-black px-6 py-3  shadow-md  w-1/2 top-0 left-1/2 ">
-      <span>{user?.name}</span>
+    <nav className="bg-gradient-to-r from-blue-600 to-purple-600 text-white fixed top-10 left-1/2 transform -translate-x-1/2 w-fit md:w-1/3 z-50 px-6 py-3 shadow-md w-1/2 top-0 left-1/2 ">
+      <span>Holovision</span>
       <button onClick={logout}>Logout</button>
     </nav>
   );
