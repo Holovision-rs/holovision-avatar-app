50c84d45a76bf640b9bbb3c69d331ca5f966e0fa
Nenad Labudovic <holovision@Nenads-Mac-Studio.local>
2025-09-01 22:08:08 +0200

    Add recharts 320

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,8 +5,14 @@ const Navbar = () => {
   const { user, logout } = useAuth();
   return (
     <nav className="flex items-center justify-between bg-gradient-to-r from-blue-600 to-purple-600 text-white fixed top-5 left-1/2 transform -translate-x-1/2 w-[90%] md:w-1/3 z-50 px-6 py-3 shadow-md w-1/2 left-1/2 ">
-      <span>Holovision</span>
-      <button onClick={logout}>Logout</button>
+  	<span>HOLOVISION</span>
+      	<button onClick={logout}>Logout</button>
+      	<button
+        onClick={() => navigate("/website")}
+        className="px-4 py-2 bg-purple-600 text-white rounded hover:bg-purple-700 transition"
+      >
+        go to Website
+      </button>
     </nav>
   );
 };
