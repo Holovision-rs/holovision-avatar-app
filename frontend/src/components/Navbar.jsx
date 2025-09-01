737d59b0a4579c36f947f95b8f04a56153de0b32
Nenad Labudovic <holovision@Nenads-Mac-Studio.local>
2025-09-01 23:41:22 +0200

    Add recharts 329

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,22 +1,31 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
-import { Globe } from 'lucide-react';
+import { Globe } from "lucide-react";
 
 const Navbar = () => {
   const { user, logout } = useAuth();
   const navigate = useNavigate();
+
   return (
-    <nav className="flex items-center justify-between bg-gradient-to-r from-blue-600 to-purple-600 text-white fixed top-5 left-1/2 transform -translate-x-1/2 w-[90%] md:w-1/3 z-50 px-6 py-3 shadow-md w-1/2 left-1/2 ">
-  	<span>HOLOVISION</span>
-      	<button onClick={logout}>Logout</button>
-		<button
-		  onClick={() => navigate("/website")}
-		  className="flex items-center gap-2 text-white"
-		>
-		  <Globe className="w-5 h-5" />
-		  <span>Go to Website</span>
-		</button>
+    <nav className="flex items-center justify-between fixed top-5 left-1/2 transform -translate-x-1/2 w-[90%] md:w-1/2 bg-gradient-to-r from-blue-600 to-purple-600 text-white px-6 py-3 shadow-md z-50 rounded-lg">
+      <span className="font-semibold text-lg">HOLOVISION</span>
+
+      <div className="flex items-center gap-4">
+        <button
+          onClick={logout}
+          className="text-white hover:underline"
+        >
+          Logout
+        </button>
+        <button
+          onClick={() => navigate("/website")}
+          className="flex items-center gap-2 text-white hover:underline"
+        >
+          <Globe className="w-5 h-5" />
+          <span>Website</span>
+        </button>
+      </div>
     </nav>
   );
 };
