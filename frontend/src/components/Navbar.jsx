537d269f4505bb708f8d3669e1297024ade5a80e
Nenad Labudovic <holovision@Nenads-Mac-Studio.local>
2025-09-01 22:21:06 +0200

    Add recharts 323

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -9,10 +9,7 @@ const Navbar = () => {
     <nav className="flex items-center justify-between bg-gradient-to-r from-blue-600 to-purple-600 text-white fixed top-5 left-1/2 transform -translate-x-1/2 w-[90%] md:w-1/3 z-50 px-6 py-3 shadow-md w-1/2 left-1/2 ">
   	<span>HOLOVISION</span>
       	<button onClick={logout}>Logout</button>
-      	<button
-        onClick={() => navigate("/website")}
-        className="px-4 py-2 bg-purple-600 text-white rounded hover:bg-purple-700 transition"
-      >
+      	<button onClick={() => navigate("/website")}>
         go to Website
       </button>
     </nav>
