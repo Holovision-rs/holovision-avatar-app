f34d6a762c59ca98e957aecf6bcf3ad2e0f9ff69
Nenad Labudović <office@holovision.rs>
2026-02-08 04:37:06 +0100

    Fix rhubarb path in Docker build

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ const app = express();
 // Body parser
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
-
+app.get("/healthz", (req, res) => res.status(200).send("ok"));
 // CORS
 app.use(
   cors({
